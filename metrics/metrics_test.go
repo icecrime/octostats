7217b8951e619aa1a5fb5c3fce50392630e470ce
David Calavera <david.calavera@gmail.com>
2015-04-09 11:11:33 -0700

    Remove buffered channel from the metrics test.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- metrics/metrics_test.go
+++ metrics/metrics_test.go
@@ -13,19 +13,10 @@ func TestCollectIssues(t *testing.T) {
 	defer fixtures.TearDown()
 
 	r := github.NewGitHubRepositoryWithClient("docker", "docker", fixtures.Client)
-	m := New(r)
-
-	feed := make(chan Metric, 100)
-
-	collectOpenedIssues(r, feed)
-	close(feed)
-
-	for e := range feed {
-		m.Items = append(m.Items, e)
-	}
+	items := collectOpenedIssues(r)
 
 	// 1 global counter + 4 issues + 4 labels
-	if len(m.Items) != 9 {
-		t.Fatalf("Expected 8 metrics but got %d\n", len(m.Items))
+	if len(items) != 9 {
+		t.Fatalf("Expected 8 metrics but got %d\n", len(items))
 	}
 }
